5db876ed02f89974d893678696c5612310e04a6f
Elena Savvina <43780662+elenasavvina@users.noreply.github.com>
2024-02-10 22:53:36 +0300

    Update ADAT_co_containing background stars.py

--- ADAT_co_containing background stars.py
+++ ADAT_co_containing background stars.py
@@ -4,46 +4,53 @@ import pandas as pd
 from astropy.io import fits
 import hdbscan
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from mpl_toolkits.mplot3d import Axes3D
 
 def plot_cluster_with_isochrone(ax, cluster_mask, data, iso_data, coord_pair, cluster_color, label):
     ax.scatter(data[coord_pair[0]][~cluster_mask], data[coord_pair[1]][~cluster_mask], color='gray', label='Other Stars', s=5, alpha=0.7)
     ax.scatter(data[coord_pair[0]][cluster_mask], data[coord_pair[1]][cluster_mask], color='red', label='Cluster Stars', s=5, alpha=0.7)
 
-    ax.plot(iso_data['G_BPmag'], iso_data['Gmag'],
-            color='darkblue', linestyle='dashed', label='Isochrone', linewidth=2)
+    ax.plot(iso_data['G_BPmag'], iso_data['Gmag'], color='darkblue', linestyle='dashed', label='Isochrone', linewidth=2)
 
     ax.set_xlabel(coord_pair[0])
     ax.set_ylabel(coord_pair[1])
     ax.legend()
 
+def plot_cluster_2d(ax, cluster_mask, data, cluster_color, label):
+    ax.scatter(data['phot_bp_mean_mag'][~cluster_mask], data['phot_rp_mean_mag'][~cluster_mask], color='gray', label='Other Stars', s=5, alpha=0.7)
+    ax.scatter(data['phot_bp_mean_mag'][cluster_mask], data['phot_rp_mean_mag'][cluster_mask], color='red', label='Cluster Stars', s=5, alpha=0.7)
+
+    ax.set_xlabel('G_BPmag')
+    ax.set_ylabel('G_RPmag')
+    ax.legend()
+    ax.set_title(f'Cluster {label}')
 
 #Load your fits data
-fits_file_path = '/your path'  #Use your path, I uploaded Gaia data from my drive
+fits_file_path = 'Your path' #Use your path, I uploaded Gaia data from my drive
 hdulist = fits.open(fits_file_path)
 data = hdulist[1].data
 hdulist.close()
 
-#Load isochrones (you are welcome to use my file "Isochrones" in csv)
+#Load isochrones (you are welcome to use my files containing Isochrones)
 iso_file_path = '/your path.csv'  #Use your path, I uploaded data from my drive
 iso_data = pd.read_csv(iso_file_path)
 
 #Coordinates for clustering
-selected_cols = ['ra', 'dec', 'pmra', 'pmdec', 'parallax', 'radial_velocity']
+selected_cols = ['ra', 'dec', 'pmra', 'pmdec', 'parallax', 'radial_velocity', 'phot_bp_mean_mag', 'phot_rp_mean_mag', 'phot_g_mean_mag']
 X = np.array([data.field(col) for col in selected_cols]).T
 
-#HDBSCAN
+#HDBSCAN for clastering
 clusterer = hdbscan.HDBSCAN(min_cluster_size=10, min_samples=5)
 labels = clusterer.fit_predict(X)
 
 #Create a dataframe to store cluster information
 df_clusters = pd.DataFrame({'label': labels})
 
-#Clusters selection conditions
+#Clasters selection conditions
 min_cluster_size = 100
 
 #Create a directory for saving images
-output_directory = 'Your path'
+output_directory = 'your path'
 os.makedirs(output_directory, exist_ok=True)
 
 #Validation and saving images
@@ -55,23 +62,33 @@ for cluster_label, cluster_size in df_clusters['label'].value_counts().items():
         valid_cluster_mask = np.isin(np.arange(len(data)), np.nonzero(cluster_mask)[0]) & valid_mask
 
         if np.sum(valid_cluster_mask) >= min_cluster_size:
-            fig, axes = plt.subplots(1, 3, figsize=(18, 5))
-            coord_pairs = [('dec', 'ra'), ('pmdec', 'pmra'), ('radial_velocity', 'parallax')]
+            #2D image after clastering
+            fig_2d, ax_2d = plt.subplots(figsize=(8, 6))
+            plot_cluster_2d(ax_2d, cluster_mask, data,
+                            cluster_color=plt.cm.jet(cluster_label / len(df_clusters['label'].unique())), label=f'Cluster {cluster_label}')
 
-            for i, coord_pair in enumerate(coord_pairs):
-                plot_cluster_with_isochrone(axes[i], cluster_mask, data, iso_data, coord_pair=coord_pair,
-                                            cluster_color=plt.cm.jet(cluster_label / len(df_clusters['label'].unique())), label=f'Cluster {cluster_label}')
+            #Saving image in TIFF
+            output_filename_2d = f'{output_directory}cluster_{cluster_label}_2d.tif'
+            plt.savefig(output_filename_2d, bbox_inches='tight', dpi=300)
+            plt.close(fig_2d)
 
-            #Add a colorbar
+            #3D image with isochrone - validation
+            fig_3d = plt.figure(figsize=(10, 8))
+            ax_3d = fig_3d.add_subplot(111, projection='3d')
+
+            plot_cluster_with_isochrone(ax_3d, cluster_mask, data, iso_data,
+                                        coord_pair=('phot_bp_mean_mag', 'phot_g_mean_mag'),
+                                        cluster_color=plt.cm.jet(cluster_label / len(df_clusters['label'].unique())), label=f'Cluster {cluster_label}')
+
+            #Adding a colorbar
             sm = plt.cm.ScalarMappable(cmap=plt.cm.jet, norm=plt.Normalize(vmin=df_clusters['label'].min(), vmax=df_clusters['label'].max()))
             sm.set_array([])
-            cbar = plt.colorbar(sm, ax=axes, orientation='vertical', fraction=0.03, pad=0.1)
+            cbar = plt.colorbar(sm, ax=ax_3d, orientation='vertical', fraction=0.03, pad=0.1)
             cbar.set_label('Cluster Label')
 
-            #Save the image (I used tiff)
-            output_filename = f'{output_directory}cluster_{cluster_label}_isochrone.tif'
-            plt.savefig(output_filename, bbox_inches='tight', dpi=300)
-            
-            plt.close()
+            #Saving 3D image in TIFF
+            output_filename_3d = f'{output_directory}cluster_{cluster_label}_isochrone_3d.tif'
+            plt.savefig(output_filename_3d, bbox_inches='tight', dpi=300)
+            plt.close(fig_3d)
 
 print(f'Your results landed here: {output_directory}')
